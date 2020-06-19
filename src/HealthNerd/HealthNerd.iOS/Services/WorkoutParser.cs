e5f922def623c8f21a2c58a782bfcd43ae59c6c7
Jon Fuller <jcfuller@sep.com>
2020-06-19 16:17:53 -0400

    add workout and record parser, accidentally forgot

---
+++ src/HealthNerd/HealthNerd.iOS/Services/WorkoutParser.cs
@@ -0,0 +1,13 @@
+﻿using HealthKit;
+using HealthKitData.Core;
+
+namespace HealthKitData.iOS
+{
+    public static class WorkoutParser
+    {
+        public static Workout ParseWorkout(HKWorkout workout)
+        {
+            return new Workout();
+        }
+    }
+}
