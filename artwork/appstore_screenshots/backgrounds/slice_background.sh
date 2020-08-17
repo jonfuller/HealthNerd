2aabb160c91bb6fb5e3d8f6e3ef64b43b04d6147
Jon Fuller <jcfuller@sep.com>
2020-08-17 15:46:27 -0400

    update screenshot machinery to have fancy backgrounds

---
+++ artwork/appstore_screenshots/backgrounds/slice_background.sh
@@ -0,0 +1,5 @@
+convert wide.png -crop 1242x2688+0+0 1.png
+convert wide.png -crop 1242x2688+1242+0 2.png
+convert wide.png -crop 1242x2688+2484+0 3.png
+convert wide.png -crop 1242x2688+3726+0 4.png
+convert wide.png -crop 1242x2688+4968+0 5.png
